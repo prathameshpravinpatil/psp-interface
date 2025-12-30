e50625a494139c9145d81fedb9dea414026ad587
Sudarshana Kharat <sudarshana.kharat@gmail.com>
2025-12-30 18:33:11 +0530

    Resolve Vite config conflict and fix build

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  }
-})
-=======
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
->>>>>>> 3995611 (Initial commit of PSP Interface)
